0247db01ffd67fa83aa0e83ee52c985a776c32d4
Oscar Vestlund <ove@lysator.liu.se>
2022-05-09 21:42:31 +0200

    Replace the brackets with full squares/raster so it looks thicc

--- main.cpp
+++ main.cpp
@@ -132,7 +132,7 @@ int main(int argc, char** argv)
 					else if(sv[i][j] >= length)
 					{
 						sv[i][j] = 0;
-						wprintw(game_arena, "[]");
+						wprintw(game_arena, "▒▒");
 					}
 					else if(sv[i][j] == -1)
 					{
@@ -143,7 +143,7 @@ int main(int argc, char** argv)
 					{
 						sv[i][j]++;
 						//wprintw(game_arena, "\u53e3");
-						wprintw(game_arena, "[]");
+						wprintw(game_arena, "██");
 					}
 				}
 			}
